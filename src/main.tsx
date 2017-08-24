7c6692211260faadea0c6ee42981ee569ed887d1
suzuki.sh <s2s@live.jp>
2017-08-24 19:30:45 +0900

    画像のアップロードに対応

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ interface ImageFormProps {
 }
 interface ImageFormState {
   image_src: string;
-  image_url: string;
   textarea_value: string;
   header: string;
 }
@@ -15,27 +14,30 @@ interface ImageFormState {
 class ImageForm extends React.Component<ImageFormProps, ImageFormState> {
   constructor(props) {
     super(props);
-    this.state = { image_src: '', image_url: '', textarea_value: '', header: '画像のURLを入力して下さい' };
+    this.state = { image_src: '', textarea_value: '', header: '画像のURLを入力して下さい' };
     this.handleChangeFile = this.handleChangeFile.bind(this);
     this.handleChangeURL = this.handleChangeURL.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChangeFile(event) {
     var files = event.target.files;
-    var image_url = window.URL.createObjectURL(files[0]);
-    this.setState({image_src: image_url});
+    this.setState({image_src: window.URL.createObjectURL(files[0])});
+
+    this.submitImage(files[0], 'application/octet-stream');
   }
   handleChangeURL(event){
-    this.setState({image_url: event.target.value});
-    this.setState({image_src: this.state.image_url});
+    this.setState({image_src: event.target.value});
   }
   handleSubmit(event) {
-    this.setState({image_src: this.state.image_url,header: "(解析しています...)"});
+    this.submitImage({url:this.state.image_src}, 'application/json');
+  }
+  submitImage(image, content_type:string) {
+    this.setState({header: "(解析しています...)", textarea_value:''});
     SuperAgent
       .post(config.computer_vision.api_url)
       .set('Ocp-Apim-Subscription-Key', config.computer_vision.api_key)
-      .set('Content-Type', 'application/json')
-      .send({url:this.state.image_url})
+      .set('Content-Type', content_type)
+      .send(image)
       .end(function(error, response){
         if (error) { return this.setState({header: response.text}); }
         var desc = JSON.parse(response.text).description.captions[0].text;
@@ -64,7 +66,7 @@ class ImageForm extends React.Component<ImageFormProps, ImageFormState> {
                 var parser = new DOMParser();
                 desc = parser.parseFromString(response_trans.text, 'text/xml').firstElementChild.textContent;
 
-                this.setState({header: desc, textarea_value: `<img alt="${desc}" src="${this.state.image_url}">`});
+                this.setState({header: desc, textarea_value: `<img alt="${desc}" src="${this.state.image_src}">`});
               }.bind(this));
           }.bind(this));
       }.bind(this));
@@ -74,7 +76,7 @@ class ImageForm extends React.Component<ImageFormProps, ImageFormState> {
       <div>
         <h1>{this.state.header}</h1>
         <form>
-          {/* <input type="file" ref="file" onChange={this.handleChangeFile} /> */}
+          <input type="file" ref="file" onChange={this.handleChangeFile} />
           <input
             type="url" placeholder="http://www.example.com/" size={40}
             onBlur={this.handleChangeURL}
